dd26af8ddc9dd325f8dcd7951a3d1eecebedfb8d
Karl Bristav <karl.bristav@solita.fi>
2021-12-01 06:57:45 +0100

    Day 1

---
+++ AoC2021/Day1.fs
@@ -0,0 +1,25 @@
+﻿namespace AoC2021
+
+open TestRunner
+
+module Day1 =
+
+    let isIncreasing ((l, r): int * int) : int = if r > l then 1 else 0
+
+    let part1 (input: string) : string =
+        input.Split "\n"
+        |> Array.map int
+        |> Array.pairwise
+        |> Array.map isIncreasing
+        |> Array.sum
+        |> string
+
+    let part2 (input: string) : string =
+        input.Split "\n"
+        |> Array.map int
+        |> Array.windowed 3
+        |> Array.map Array.sum
+        |> Array.pairwise
+        |> Array.map isIncreasing
+        |> Array.sum
+        |> string
---
+++ AoC2021/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+open AoC2021
+open TestRunner
+
+[<EntryPoint>]
+let main argv =
+    
+    //let res = TestRunner.makeComparison TestRunner.exampleResults1_1 (List.map Day1.part1 TestRunner.examples1_1)
+    //let res = Day1.part1 TestRunner.input1
+    
+    runExamplesAndMain TestRunner.examples1_1 TestRunner.exampleResults1_1 TestRunner.input1 Day1.part1 "Day 1 part 1"
+    runExamplesAndMain TestRunner.examples1_2 TestRunner.exampleResults1_2 TestRunner.input1 Day1.part2 "Day 1 part 2"
+
+    //printfn "%A" res
+    0 // return an integer exit code
