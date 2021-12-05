715548df0f62fc7242114c075bb399fee8ae3366
Karl Bristav <karl.bristav@gmail.com>
2021-12-05 10:31:37 +0100

    Day 5

---
+++ AoC2021/Day5.fs
@@ -0,0 +1,118 @@
+﻿namespace AoC2021
+
+open Utils
+open FSharp.Collections
+
+module Day5 =
+
+    type OverlapMap = Map<(int * int), int>
+    type Line = ((int * int) * (int * int))
+
+    let makePoint (pointPair: string) : (int * int) =
+        let points = pointPair.Split ","
+        (int points.[0], int points.[1])
+
+    let processInput (line: string) : Line =
+        line.Split " -> "
+        |> Array.map makePoint
+        |> fun ps -> (ps.[0], ps.[1])
+
+    let makeLinePoints (line: Line) : (int * int) [] =
+        let ((x1, y1), (x2, y2)) = line
+
+        if x1 = x2 then
+            let ys =
+                if y1 < y2 then
+                    [| y1 .. y2 |]
+                else
+                    Array.rev [| y2 .. y1 |]
+
+            let xs = Array.replicate ys.Length x1
+            Array.zip xs ys
+        else if y1 = y2 then
+            let xs =
+                if x1 < x2 then
+                    [| x1 .. x2 |]
+                else
+                    Array.rev [| x2 .. x1 |]
+
+            let ys = Array.replicate xs.Length y1
+            Array.zip xs ys
+        else
+            let xs =
+                if x1 < x2 then
+                    [| x1 .. x2 |]
+                else
+                    Array.rev [| x2 .. x1 |]
+
+            let ys =
+                if y1 < y2 then
+                    [| y1 .. y2 |]
+                else
+                    Array.rev [| y2 .. y1 |]
+
+            Array.zip xs ys
+
+    let isStraight (line: Line) : bool =
+        let ((x1, y1), (x2, y2)) = line
+
+        x1 = x2 || y1 = y2
+
+    let isStraightOrDiagonal (line: Line) : bool =
+        let ((x1, y1), (x2, y2)) = line
+
+        isStraight line || abs (x1 - x2) = abs (y1 - y2)
+
+    let rec compareLines (map: OverlapMap) (diags: bool) (lines: Line []) : OverlapMap =
+        match lines with
+        | [||] -> map
+        | _ ->
+            let isValidLine =
+                if diags then
+                    isStraightOrDiagonal
+                else
+                    isStraight
+
+            let firstLine = Array.head lines
+            let restOfLines = Array.tail lines
+
+            let newMap =
+                if isValidLine firstLine then
+                    Array.fold
+                        (fun m ov ->
+                            Map.change
+                                ov
+                                (fun x ->
+                                    match x with
+                                    | Some i -> Some(i + 1)
+                                    | None -> Some 1)
+                                m)
+                        map
+                        (makeLinePoints firstLine)
+                else
+                    map
+
+
+            compareLines newMap diags restOfLines
+
+    let countOverlaps (map: OverlapMap) : int =
+        map
+        |> Map.values
+        |> Seq.filter (fun i -> i >= 2)
+        |> Seq.length
+
+    let part1 (input: string) : string =
+        input
+        |> lines
+        |> Array.map processInput
+        |> compareLines Map.empty false
+        |> countOverlaps
+        |> string
+
+    let part2 (input: string) : string =
+        input
+        |> lines
+        |> Array.map processInput
+        |> compareLines Map.empty true
+        |> countOverlaps
+        |> string
--- AoC2021/TestRunner.fs
+++ AoC2021/TestRunner.fs
@@ -1,6 +1,7 @@
 ﻿namespace AoC2021
 
 open System.IO
+open System.Diagnostics
 
 module TestRunner =
 
@@ -19,12 +20,7 @@ module TestRunner =
     let printStatus ((res, expectedRes, success): string * string * bool) =
         printfn "%s! Got %s, expected %s." (if success then "Success" else "Failure") res expectedRes
 
-    let runExamplesAndMain examples expectedResults realInput (func: string -> string) title =
-        let resultList =
-            examples
-            |> Array.map func
-            |> makeComparison expectedResults
-
+    let runExamplesAndMain (examples: string []) expectedResults realInput (func: string -> string) title =
         printfn title
 
         if examples.Length = 0 then
@@ -32,14 +28,25 @@ module TestRunner =
         else
             printfn "Running and verifying examples before the real input..."
 
+        let resultList =
+            examples
+            |> Array.map func
+            |> makeComparison expectedResults
+
         resultList |> Array.map printStatus |> ignore
 
         let examplesSuccessful =
             resultList
             |> Array.fold (fun b1 (_, _, b2) -> b1 && b2) true
 
+
+
         if examplesSuccessful then
+            let timer = new Stopwatch()
+            timer.Start()
             printfn "All examples were successful. Result from real input: %s" (func realInput)
+            timer.Stop()
+            printfn "Time elapsed: %A" timer.Elapsed
         else
             printfn "Some examples were not successful. PLEASE DO BETTER"
 
@@ -82,8 +89,8 @@ module TestRunner =
     let exampleResults3_2 = [| "230" |]
 
 
-    // Day 3
-    let input4 = getInput 4
+    // Day 4
+    //let input4 = getInput 4
 
     let examples4_1 =
         [| "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n\n22 13 17 11  0\n 8  2 23  4 24\n21  9 14 16  7\n 6 10  3 18  5\n 1 12 20 15 19\n\n 3 15  0  2 22\n 9 18 13 17  5\n19  8  7 25 23\n20 11 10 24  4\n14 21 16 12  6\n\n14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7" |]
@@ -93,3 +100,16 @@ module TestRunner =
     let examples4_2 = examples4_1
 
     let exampleResults4_2 = [| "1924" |]
+
+
+    // Day 5
+    let input5 = getInput 5
+
+    let examples5_1 =
+        [| "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2" |]
+
+    let exampleResults5_1 = [| "5" |]
+
+    let examples5_2 = examples5_1
+
+    let exampleResults5_2 = [| "12" |]
