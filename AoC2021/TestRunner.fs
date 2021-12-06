b07e45919d219756e88b943668080dddfd0786b2
Karl Bristav <karl.bristav@solita.fi>
2021-12-06 07:53:59 +0100

    TestRunner improvements

--- AoC2021/TestRunner.fs
+++ AoC2021/TestRunner.fs
@@ -6,12 +6,12 @@ open System.Diagnostics
 module TestRunner =
 
     let getInput day =
-        let filename = $"input_day{day}.txt"
+        let filename = $"..\..\..\inputs\input_day{day}.txt"
 
         if File.Exists filename then
             File.ReadAllText filename
         else
-            failwith $"Input file input_day{day}.txt not found"
+            failwith $"Input file {filename} not found"
 
     let makeComparison (expectedResults: string []) (results: string []) =
         Array.zip results expectedResults
@@ -42,16 +42,20 @@ module TestRunner =
 
 
         if examplesSuccessful then
+            printfn "All examples were successful, running the real input..."
             let timer = new Stopwatch()
             timer.Start()
-            printfn "All examples were successful. Result from real input: %s" (func realInput)
+            printfn "Result from real input: %s" (func realInput)
             timer.Stop()
             printfn "Time elapsed: %A" timer.Elapsed
         else
             printfn "Some examples were not successful. PLEASE DO BETTER"
 
+
+        printfn ""
+
     // Day1
-    //let input1 = getInput 1
+    let input1 = getInput 1
 
     let examples1_1 =
         [| "199\n200\n208\n210\n200\n207\n240\n269\n260\n263" |]
@@ -64,7 +68,7 @@ module TestRunner =
 
 
     // Day 2
-    //let input2 = getInput 2
+    let input2 = getInput 2
 
     let examples2_1 =
         [| "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2" |]
@@ -77,7 +81,7 @@ module TestRunner =
 
 
     // Day 3
-    //let input3 = getInput 3
+    let input3 = getInput 3
 
     let examples3_1 =
         [| "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010" |]
@@ -90,7 +94,7 @@ module TestRunner =
 
 
     // Day 4
-    //let input4 = getInput 4
+    let input4 = getInput 4
 
     let examples4_1 =
         [| "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n\n22 13 17 11  0\n 8  2 23  4 24\n21  9 14 16  7\n 6 10  3 18  5\n 1 12 20 15 19\n\n 3 15  0  2 22\n 9 18 13 17  5\n19  8  7 25 23\n20 11 10 24  4\n14 21 16 12  6\n\n14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7" |]
@@ -103,7 +107,7 @@ module TestRunner =
 
 
     // Day 5
-    //let input5 = getInput 5
+    let input5 = getInput 5
 
     let examples5_1 =
         [| "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2" |]
