d878da1bd6448fc77fcf9a9ca09f8774b3b46043
Karl Bristav <karl.bristav@gmail.com>
2021-12-13 07:12:30 +0100

    Day 13

---
+++ AoC2021/Day13.fs
@@ -0,0 +1,90 @@
+﻿namespace AoC2021
+
+open Utils
+
+module Day13 =
+
+    type Paper = Map<int * int, bool>
+
+    type FoldAxis =
+        | X of int
+        | Y of int
+
+    let markPaper (coords: string []) : Paper =
+        coords
+        |> Array.map commas
+        |> Array.map (Array.map int)
+        |> Array.map twoArrayToTuple
+        |> Array.map (fun p -> p, true)
+        |> Map.ofArray
+
+    let parseFold (fold: string) : FoldAxis =
+        let (_, e) =
+            fold.Split("fold along ") |> twoArrayToTuple
+
+        let (axis, loc) = e.Split("=") |> twoArrayToTuple
+
+        match axis with
+        | "x" -> X(int loc)
+        | "y" -> Y(int loc)
+        | _ -> failwith "fold fail!"
+
+    let transformCoord (fold: FoldAxis) (coord: ((int * int) * bool)) : ((int * int) * bool) =
+        let ((x, y), b) = coord
+
+        match fold with
+        | X a ->
+            if x > a then
+                ((a - (x - a), y), b)
+            else
+                ((x, y), b)
+        | Y a ->
+            if y > a then
+                ((x, a - (y - a)), b)
+            else
+                ((x, y), b)
+
+    let foldPaper (fold: FoldAxis) (paper: Paper) : Paper =
+        Map.toArray paper
+        |> Array.map (transformCoord fold)
+        |> Map.ofArray
+
+    let part1 (input: string) : string =
+        let (coords, foldStrings) = input.Split("\n\n") |> twoArrayToTuple
+
+        let folds =
+            foldStrings |> lines |> Array.map parseFold
+
+        let foldedPaper =
+            coords
+            |> lines
+            |> markPaper
+            |> foldPaper (Array.head folds)
+
+        foldedPaper.Count |> string
+
+    let showPaper (keys: (int * int) []) : unit =
+        let (xs, ys) = Array.unzip keys
+
+        for y in 0 .. (Array.max ys) do
+            for x in 0 .. (Array.max xs) do
+                if Array.contains (x, y) keys then
+                    printf "#"
+                else
+                    printf " "
+
+            printf "\n"
+
+    let part2 (input: string) : string =
+        let (coords, folds) = input.Split("\n\n") |> twoArrayToTuple
+        let paper = coords |> lines |> markPaper
+
+        folds
+        |> lines
+        |> Array.map parseFold
+        |> Array.fold (fun p f -> foldPaper f p) paper
+        |> Map.keys
+        |> Array.ofSeq
+        |> showPaper
+
+        ""
--- AoC2021/TestRunner.fs
+++ AoC2021/TestRunner.fs
@@ -207,3 +207,16 @@ module TestRunner =
     let examples12_2 = examples12_1
 
     let exampleResults12_2 = [| "36"; "103"; "3509" |]
+
+
+    // Day 13
+    let input13 = getInput 13
+
+    let examples13_1 =
+        [| "6,10\n0,14\n9,10\n0,3\n10,4\n4,11\n6,0\n6,12\n4,1\n0,13\n10,12\n3,4\n3,0\n8,4\n1,10\n2,14\n8,10\n9,0\n\nfold along y=7\nfold along x=5" |]
+
+    let exampleResults13_1 = [| "17" |]
+
+    let examples13_2 = examples13_1
+
+    let exampleResults13_2 = [| "" |]
